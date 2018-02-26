167044f0bbaeb8e3b7690df6715166d83f32a34f
Bhupen Khanolkar <bhupenk@gmail.com>
2018-02-26 14:20:24 -0600

    Added default help

--- main.go
+++ main.go
@@ -9,10 +9,19 @@ import (
 
 func main() {
 
-	toZone := flag.String("zone", "America/Chicago", "time zone to convert to (America/Chicago)")
+	toZone := flag.String("zone", "US/Central", "time zone to convert to (US/Central)")
 	flag.Parse()
 
 	layout := "2006-01-02 15:04:05"
+
+	if len(flag.Args()) < 1 {
+		fmt.Println(`
+	Usage : convertTimeFromUTC <--zone=""> <YYYY-MM-DD HH:mm::ss>
+	Optional zone: timezone to convert to (default: US/Central)
+			`)
+		os.Exit(0)
+	}
+
 	str := flag.Args()[0]
 	t, err := time.Parse(layout, str)
 	if err != nil {
